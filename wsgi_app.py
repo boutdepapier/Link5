7a458381894f8fdd4fc64f060e803e59ff949f1a
lcloud <benjamin@tacquet.com>
2012-04-01 15:18:38 +0200

    updated uwsgi conf

--- wsgi_app.py
+++ wsgi_app.py
@@ -1,11 +1,11 @@
-# wsgi_app.py
-import sys
 import os
+import sys
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-import django.core.handlers.wsgi
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
 
